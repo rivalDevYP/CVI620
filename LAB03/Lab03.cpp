a08be5a4000764e8ce2ee3baf0f4bf524302d5ca
rivalDevYP <yathavanparamesh1998@hotmail.com>
2020-01-20 13:03:54 -0500

    updated user input

--- LAB03/Lab03.cpp
+++ LAB03/Lab03.cpp
@@ -4,7 +4,7 @@
 #include <opencv2/core/types.hpp>
 #include <iostream>
 #include <string>
-#include <dirent.h>
+#include <cstdio>
 
 #define HEIGHT 480
 #define WIDTH 640
@@ -14,14 +14,18 @@ int main()
     cv::Mat black_box(HEIGHT, WIDTH, CV_8UC3, cv::Scalar(0, 0, 0));
     bool input_not_valid = true;
     double x1 = 0.0, x2 = 0.0, y1 = 0.0, y2 = 0.0;
-    cv::viz::Color selectedColor;
+    std::string x1_str = "", y1_str = "", x2_str = "", y2_str = "";
+    cv::Vec3b selectedColor;
     char userColorSelection = '\0';
 
     while (input_not_valid)
     {
-        std::cout << "please enter a coordinate for x1: ";
-        std::cin >> x1;
-        if (x1 < 0 || x1 > HEIGHT)
+        std::cout << "Enter the coordinates of the top left corner (x y): ";
+        std::cin >> x1_str >> y1_str;
+        x1 = std::stod(x1_str);
+        y1 = std::stod(y1_str);
+        
+        if ((x1 < 0 || x1 > HEIGHT) || (y1 < 0 || y1 > HEIGHT))
         {
             std::cout << "ERROR => please enter a valid value" << std::endl;
             continue;
@@ -34,39 +38,12 @@ int main()
 
     while (input_not_valid)
     {
-        std::cout << "please enter a coordinate for y1: ";
-        std::cin >> y1;
-        if (y1 < 0 || y1 > HEIGHT)
-        {
-            std::cout << "ERROR => please enter a valid value" << std::endl;
-            continue;
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    while (input_not_valid)
-    {
-        std::cout << "please enter a coordinate for x2: ";
-        std::cin >> x2;
-        if (x2 < 0 || x2 > HEIGHT)
-        {
-            std::cout << "ERROR => please enter a valid value" << std::endl;
-            continue;
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    while (input_not_valid)
-    {
-        std::cout << "please enter a coordinate for y2: ";
-        std::cin >> y2;
-        if (y2 < 0 || y2 > HEIGHT)
+        std::cout << "Enter the coordinates of the bottom right corner (x y): ";
+        std::cin >> x2_str >> y2_str;
+        x1 = std::stod(x2_str);
+        y1 = std::stod(y2_str);
+        
+        if ((x2 < 0 || x2 > HEIGHT) || (y2 < 0 || y2 > HEIGHT))
         {
             std::cout << "ERROR => please enter a valid value" << std::endl;
             continue;
@@ -78,44 +55,35 @@ int main()
     }
 
     /* FOR TESTING PURPOSES, REMOVE BEFORE FINAL SUBMISSION */
-    std::cout << x1 << std::endl;
-    std::cout << x2 << std::endl;
-    std::cout << y1 << std::endl;
-    std::cout << y2 << std::endl;
+    // std::cout << x1 << std::endl;
+    // std::cout << x2 << std::endl;
+    // std::cout << y1 << std::endl;
+    // std::cout << y2 << std::endl;
 
-    std::cout << "Please specify a color..." << std::endl;
+    std::cout << "Enter a letter for color (x:random, r:red, g:green, b:blue, k:black, w:white, y:yellow, c:cyan, m:magenta): ";
     std::cin >> userColorSelection;
 
     switch (userColorSelection)
     {
     case 'k':
-        selectedColor = cv::viz::Color::black();
+        selectedColor = cv::Vec3b(0, 0, 0);
     case 'w':
-        selectedColor = cv::viz::Color::white();
+        selectedColor = cv::Vec3b(0, 0, 255);
     case 'r':
-        selectedColor = cv::viz::Color::red();
+        selectedColor = cv::Vec3b(0, 255, 0);
     case 'g':
-        selectedColor = cv::viz::Color::green();
+        selectedColor = cv::Vec3b(255, 0, 0);
     case 'b':
-        selectedColor = cv::viz::Color::blue();
+        selectedColor = cv::Vec3b(0, 255, 255);
     case 'c':
-        selectedColor = cv::viz::Color::cyan();
+        selectedColor = cv::Vec3b(255, 0, 255);
     case 'y':
-        selectedColor = cv::viz::Color::yellow();
+        selectedColor = cv::Vec3b(255, 255, 0);
     case 'm':
-        selectedColor = cv::viz::Color::magenta();
+        selectedColor = cv::Vec3b(255, 255, 255);
     case 'x':
-        selectedColor = cv::Scalar(rand() % 255, rand() % 255);
+        selectedColor = cv::Vec3b(rand() % 256, rand() % 256, rand() % 256);
     };
-
-//     Black (0, 0, 0)
-// Red (0, 0, 255)
-// Green (0, 255, 0)
-// Blue (255, 0, 0)
-// Yellow (0, 255, 255)
-// Magenta (255, 0, 255)
-// Cyan (255, 255, 0)
-// White (255, 255, 255)
 
     for (int index = y1; index < y2; index++)
     {
