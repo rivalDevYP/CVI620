25b2c477c4f4d1860b84eaed1e3ccfcac212f884
rivalDevYP <yathavanparamesh1998@hotmail.com>
2020-02-10 19:49:07 -0500

    lab 5 fixed

--- LAB05/Lab05.cpp
+++ LAB05/Lab05.cpp
@@ -10,32 +10,35 @@ We, (Gabriella Ko, Patrick O'reilly, Yathavan Parameshwaran), declare that the a
 #include <vector>
 #include <numeric>
 
-/* code for generateNoise acquired from 'A Practical Introduction to COMPUTER VISION WITH OPENCV by Kenneth Dawson-Howe' */
-cv::Mat &generateNoise(cv::Mat &incomingImage, double incomingNoisePercentage)
+cv::Mat generateNoise(cv::Mat &incomingImage, float incomingNoisePercentage)
 {
-    int imageRows = incomingImage.rows;
-    int imageColumns = incomingImage.cols;
-    int imageChannels = incomingImage.channels();
-
-    int noisePoints = (int)(((double)imageRows * imageColumns * imageChannels) * incomingNoisePercentage / 100.0);
-
-    for (int index = 0; index < noisePoints; index++)
+    cv::Mat noisyImg = incomingImage;
+    cv::Mat x(incomingImage.rows, incomingImage.cols, CV_32FC1);
+    cv::RNG rngObj;
+    rngObj.fill(x, cv::RNG::UNIFORM, 0.0f, 1.0f);
+    for (int rowIter = 0; rowIter < incomingImage.rows; rowIter++)
     {
-        int row = rand() % imageRows;
-        int column = rand() % imageColumns;
-        int channel = rand() % imageChannels;
+        for (int colIter = 0; colIter < incomingImage.cols; colIter++)
+        {
+            if (x.at<float>(rowIter, colIter) < incomingNoisePercentage)
+            {
+                float y = rngObj.uniform(0.0f, 1.0f);
+                
+                cv::Vec3b &pixel = noisyImg.at<cv::Vec3b>(rowIter, colIter);
 
-        uchar *pixel = incomingImage.ptr<uchar>(row) + (column * imageChannels) + channel;
-
-        *pixel = (rand() % 2 == 1) ? 200 : 0;
+                pixel[0]  = 0 + (y * (255 - 0));
+                pixel[1]  = 0 + (y * (255 - 0));
+                pixel[2]  = 0 + (y * (255 - 0));
+            }
+        }
     }
 
-    return incomingImage;
+    return noisyImg;
 }
 
 int main()
 {
-    double userProbabilityInput = 0.0;
+    float userProbabilityInput = 0.0;
 
     // a. Open a color image and display.
 
@@ -43,7 +46,7 @@ int main()
     myImage = cv::imread("dell_latitude.jpg", cv::IMREAD_COLOR);
     cv::namedWindow("dell latitude", cv::WINDOW_AUTOSIZE);
     cv::imshow("dell latitude", myImage);
-    cv::waitKey(0);
+    cv::waitKey(1000);
 
     // b. Ask for a probability value between 0 and 1, and implement the salt and pepper noise with the given probability. Create a noisy image with p=0.1. Display the noisy image.
 
@@ -52,7 +55,7 @@ int main()
     cv::Mat noisyImage = generateNoise(myImage, userProbabilityInput);
     cv::imwrite("noisy_image.jpg", noisyImage);
     cv::imshow("noisy image", noisyImage);
-    cv::waitKey(0);
+    cv::waitKey(1000);
 
     // c. Smooth the above noisy image using a 3 x 3 box kernel. Display the blurred image.
 
@@ -60,7 +63,7 @@ int main()
     cv::blur(noisyImage, smoothedImage, cv::Size(3, 3));
     cv::imwrite("smoothed_image.jpg", smoothedImage);
     cv::imshow("smoothed image", smoothedImage);
-    cv::waitKey(0);
+    cv::waitKey(1000);
 
     // d. Choose a pixel and output the pixel’s blue value, as well as its neighbors’.
 
@@ -76,7 +79,7 @@ int main()
     blueVals.push_back((short)noisyImage.at<cv::Vec3b>(pos1 + 1, pos2)[0]);
     blueVals.push_back((short)noisyImage.at<cv::Vec3b>(pos1 + 1, pos2 + 1)[0]);
 
-    std::cout << "Printing blue values of pixel at (500,500) and it's neighbors from NOISY IMAGE..." << std::endl;
+    std::cout << "\nPrinting blue values of pixel at (500,500) and it's neighbors from NOISY IMAGE..." << std::endl;
     for (std::vector<short>::iterator i = blueVals.begin(); i != blueVals.end(); i++)
     {
         std::cout << *i << std::endl;
@@ -87,34 +90,21 @@ int main()
     std::cout.precision(15);
     std::cout << "I predict that the blurred pixel value will be " << std::accumulate(blueVals.begin(), blueVals.end(), 0) << "/" << blueVals.size() << " = " << std::fixed << (std::accumulate(blueVals.begin(), blueVals.end(), (double)0) / blueVals.size()) << std::endl;
 
-    // Then check the value for the blurred image. Are they the same?
-
     blueVals.clear();
 
-    blueVals.push_back((short)smoothedImage.at<cv::Vec3b>(pos1 - 1, pos2 - 1)[0]);
-    blueVals.push_back((short)smoothedImage.at<cv::Vec3b>(pos1 - 1, pos2)[0]);
-    blueVals.push_back((short)smoothedImage.at<cv::Vec3b>(pos1 - 1, pos2 + 1)[0]);
-    blueVals.push_back((short)smoothedImage.at<cv::Vec3b>(pos1, pos2 - 1)[0]);
-    blueVals.push_back((short)smoothedImage.at<cv::Vec3b>(pos1, pos2)[0]);
-    blueVals.push_back((short)smoothedImage.at<cv::Vec3b>(pos1, pos2 + 1)[0]);
-    blueVals.push_back((short)smoothedImage.at<cv::Vec3b>(pos1 + 1, pos2 - 1)[0]);
-    blueVals.push_back((short)smoothedImage.at<cv::Vec3b>(pos1 + 1, pos2)[0]);
-    blueVals.push_back((short)smoothedImage.at<cv::Vec3b>(pos1 + 1, pos2 + 1)[0]);
+    // Then check the value for the blurred image. Are they the same?
 
-    std::cout << "\n\nPrinting blue values of pixel at (500,500) and it's neighbors from SMOOTHED IMAGE..." << std::endl;
-    for (std::vector<short>::iterator i = blueVals.begin(); i != blueVals.end(); i++)
-    {
-        std::cout << *i << std::endl;
-    }
+    std::cout << "\nBlue value of pixel at (500,500) from SMOOTHED IMAGE: "<< (short)smoothedImage.at<cv::Vec3b>(pos1, pos2)[0] << std::endl;
+
 
     // e. Filter the above noisy image using a 3 x 3 bilinear filter and display the result.
 
     cv::Mat filteredImage;
-    cv::Matx<float, 3, 3> myKernel = cv::Matx<float, 3, 3>::all(1 / 9.0f);
+    cv::Matx<float, 3, 3> myKernel = cv::Matx<float, 3, 3>::all((1 / 16.0f));
     cv::filter2D(noisyImage, filteredImage, CV_8U, myKernel);
     cv::imwrite("box_filtered_image.jpg", filteredImage);
     cv::imshow("box filtered image", filteredImage);
-    cv::waitKey(0);
+    cv::waitKey(1000);
 
     // f. De-noise the noisy image using a median filter and display the resulting image.
 
@@ -122,7 +112,7 @@ int main()
     cv::medianBlur(noisyImage, medianFilteredImg, 3);
     cv::imwrite("median_filtered_image.jpg", medianFilteredImg);
     cv::imshow("median filtered image", medianFilteredImg);
-    cv::waitKey(0);
+    cv::waitKey(1000);
 
     // g. Compare the results of the above filters. Which filter works better for de-noising?
 
