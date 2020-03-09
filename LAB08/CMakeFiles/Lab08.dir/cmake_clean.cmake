3a01943140a96fd1406b7bcb98bb286f588c1a1a
rivalDevYP <yathavanparamesh1998@hotmail.com>
2020-03-09 11:01:41 -0400

    added lab 8

---
+++ LAB08/CMakeFiles/Lab08.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab08.dir/Lab08.cpp.o"
+  "Lab08"
+  "Lab08.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab08.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LAB08/Lab08.cpp
@@ -0,0 +1,18 @@
+/*
+
+We, (Gabriella Ko, Patrick O'reilly, Yathavan Parameshwaran), declare that the attached assignment is our own work in accordance with the Seneca Academic Policy. We have not copied any part of this assignment, manually or electronically, from any other source including web sites, unless specified as references. We have not distributed our work to other students.
+
+*/
+
+#include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+#include <vector>
+#include <numeric>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
