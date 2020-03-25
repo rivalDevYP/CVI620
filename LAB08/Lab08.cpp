678a6b478a5d53d12ae6fabfbedef002a891d15d
rivalDevYP <yathavanparamesh1998@hotmail.com>
2020-03-25 19:23:31 -0400

    worked on lab

--- LAB08/Lab08.cpp
+++ LAB08/Lab08.cpp
@@ -29,19 +29,6 @@ int calcPR(bool *pred, bool *gt, int len, double *P, double *R)
     //
     //      Return 1 if successful, 0 otherwise
 
-    // Instructions:
-    //      Write a loop and go through all elements of pred and gt;
-    //      update values of TP, FP, etc.
-    //      e.g:
-    //          if (*(pred + j) && !(*(gt + j)))
-    //          {
-    //              FP++;
-    //          }
-    //      Then use formulas from class to calculate precision and recall
-
-    // recall = tp / (tp + fn)
-    // precision = tp / (tp + fp)
-
     // TP (true positives):
     //     Number of samples identified correctly as belonging to a class/ category
     // FP (false positive): (false alarms!)
@@ -61,31 +48,31 @@ int calcPR(bool *pred, bool *gt, int len, double *P, double *R)
 
         // std::cout << "reached" << std::endl;
 
-        for (int index = 0; index < 19; index++)
+        for (int index = 0; index < len; index++)
         {
             if ((*(pred + index)) && !(*(gt + index)))
             {
                 FP++;
             }
-            if ((*(pred + index)) && (*(gt + index)))
+            else if ((*(pred + index)) && (*(gt + index)))
             {
                 TP++;
             }
-            if (!(*(pred + index)) && (*(gt + index)))
-            {
-                TN++;
-            }
-            if (!(*(pred + index)) && !(*(gt + index)))
+            else if (!(*(pred + index)) && (*(gt + index)))
             {
                 FN++;
+            }
+            else if (!(*(pred + index)) && !(*(gt + index)))
+            {
+                TN++;
             }
         }
 
         // recall = tp / (tp + fn)
         // precision = tp / (tp + fp)
 
-        *R = (double) TP / (TP + FN);
-        *P = (double) TP / (TP + FP);
+        *R = (double)TP / (TP + FN);
+        *P = (double)TP / (TP + FP);
 
         return 1;
     }
@@ -99,60 +86,47 @@ int calcPR(bool *pred, bool *gt, int len, double *P, double *R)
 double calcFb(double P, double R, double beta)
 {
     // Fβ = (1 + (β^2)) * (P*R) / ((β^2) * P) + R
-    double answer = (1 + pow(beta, 2)) * (P * R) / (pow(beta, 2) * P) + R;
-
-    return answer;
+    return (1 + pow(beta, 2)) * ((P * R) / ((pow(beta, 2) * P) + R));
 }
 
-/** read CSV files (added by student) */
-bool *readCSV(std::string incomingFileName)
+void readBooleanCSV(std::string incomingFileName, bool *incomingPtr)
 {
-    /*
-        Write function(s) to read alg_bin.csv as the classifier predictions and gt.csv as the 
-        ground truth. In these files, each row belongs to a data sample. The value ‘1’ means 
-        belonging to the class (true), while ‘0’ means not belonging to the class (false).
-    */
-
     std::ifstream csv_file(incomingFileName);
 
-    int i = 0;
-    bool readLine = '\0';
-
-    // read the file to get length of the file
     if (csv_file.is_open())
     {
+        int i = 0;
+        while (!csv_file.eof() && i < 20)
+        {
+            csv_file >> incomingPtr[i++];
+        }
+    }
+}
+
+void readDoubleDSV(std::string incomingFileName, double *incomingPtr)
+{
+    std::ifstream csv_file(incomingFileName);
+
+    static double d_buffer[19];
+
+    if (csv_file.is_open())
+    {
+        int i = 0;
         while (!csv_file.eof())
         {
-            csv_file >> readLine;
-            myFileLength++;
+            csv_file >> incomingPtr[i++];
         }
     }
-
-    myFileLength = myFileLength - 1; // because of the extra blank line in the file
-
-    // initialize array to store file's data
-    static bool fileData[20];
-
-    // go back to beginning of file
-    csv_file.clear();
-    csv_file.seekg(0, std::ios::beg);
-
-    // read file again, this time storing the data in the array
-    if (csv_file.is_open())
-    {
-        for (int i = 0; i < myFileLength; i++)
-        {
-            csv_file >> fileData[i];
-        }
-    }
-
-    return fileData;
 }
 
 /** convert the response of a continuous classifier (A) to a Boolean response (B) given a threshold (thresh) */
 void thresh_v(double *A, bool *B, int len, double thresh)
 {
     // If the response value is more than the threshold, convert it to true; otherwise, convert to false.
+    for (int i = 0; i < len; i++)
+    {
+        *(B + i) = (*(A + i)) > thresh;
+    }
 }
 
 /** Write a function to read the continuous response in alg_dbl.csv */
@@ -163,67 +137,150 @@ void readResponse()
 /** converts an image to a Boolean array. If the pixel value is more than a threshold, convert it to true; otherwise, convert to false */
 void thresh_img(cv::Mat img, bool *B, double thresh)
 {
+    // *(B + j) = (*(A + j) > thresh);
 }
 
-void evalBinClass()
+// Part I: Evaluating a binary classifier
+void option_1()
 {
-    double P, R;
-    if (calcPR(readCSV("alg_bin.csv"), readCSV("gt.csv"), 20, &P, &R))
+    const unsigned int len = 20;
+    double P{0.0}, R{0.0};
+
+    bool *ptr1 = new bool[len];
+    bool *ptr2 = new bool[len];
+
+    readBooleanCSV("alg_bin.csv", ptr1);
+    readBooleanCSV("gt.csv", ptr2);
+
+    if (calcPR(ptr1, ptr2, len, &P, &R))
     {
-        std::cout << "Part I: F1= " << std::fixed << std::setprecision(4) << calcFb(P, R, 1) << std::endl;
+        std::cout << "Part I: F1= " << std::fixed << std::setprecision(4) << calcFb(P, R, 1) << "\n\n"
+                  << std::endl;
     }
+    else
+    {
+        std::cout << "\nerror while performing evaluation...\n"
+                  << std::endl;
+    }
+
+    delete[] ptr1;
+    delete[] ptr2;
 }
 
-void evalBinContRes()
+// Part II: Evaluating a binary classifier with a continuous response
+void option_2()
 {
 
+    // Note from Professor
+    // *******************
+
+    // Hi Yathavan,
+
+    // Please see the comments in the lab instructions. You want to check
+    // every element in array A and see if it is more than a threshold or
+    // not. If it is, you set the corresponding element in B to TRUE,
+    // otherwise to FALSE. It seems like you have already done that, so
+    // just put that in the thresh_v function. Then call it for applying
+    // to the array read from alg_dbl.csv.
+
+    // Please let me know if you are still stuck.
+    // Cheers,
+    // Vida
+
+    const int csv_len = 20;
+    const int thres_len = 9;
+
+    double F1 = 0.0, threshold[thres_len] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9}, P = 0, R = 0, selectedThreshold;
+
+    bool *ptr1 = new bool[csv_len];
+    bool *ptr2 = new bool[csv_len];
+    double *ptr3 = new double[csv_len];
+
+    std::ofstream pr_csv_file;
+    pr_csv_file.open("PR.csv");
+
+    readBooleanCSV("alg_bin.csv", ptr1);
+    readBooleanCSV("gt.csv", ptr2);
+    readDoubleDSV("alg_dbl.csv", ptr3);
+
+    for (int index = 0; index < thres_len; index++)
+    {
+        thresh_v(ptr3, ptr1, thres_len, threshold[index]);
+        thresh_v(ptr3, ptr2, thres_len, threshold[index]);
+        if (calcPR(ptr1, ptr2, csv_len, &P, &R))
+        {
+            double fb = calcFb(P, R, 1);
+            pr_csv_file << fb << "," << threshold[index] << std::endl;
+            if (fb > F1)
+            {
+                F1 = fb;
+                selectedThreshold = threshold[index];
+            }
+        }
+        else
+        {
+            std::cout << "BUG" << std::endl;
+        }
+        
+    }
+
+    std::cout << "Part II: max F1 = " << F1 << " at threshold = " << selectedThreshold << std::endl;
+
+    delete[] ptr1;
+    delete[] ptr2;
+    delete[] ptr3;
 }
 
-void evalImgBasClass()
+// Part III: Evaluating image-based classifiers
+void option_3()
 {
-
 }
 
-void evalImgBasClassContRes()
+// Part IV: Evaluating image-based classifiers with continuous responses
+void option_4()
 {
-    
 }
 
 int main()
 {
-    int userSelection = 0;
-
-    std::cout << "Please select one of the following options: " << std::endl;
-    std::cout << "\n(1) Evaluating a binary classifier"
-              << "\n(2) Evaluating a binary classifier with a continuous response"
-              << "\n(3) Evaluating image-based classifiers"
-              << "\n(4) Evaluating image-based classifiers with continuous responses"
-              << "\n(5) Exit Program"
-              << std::endl;
-
-    std::cout << "Please enter a selection: ";
-    std::cin >> userSelection;
-
-    switch (userSelection)
+    while (1)
     {
-    case 1:
-        evalBinClass();
-        break;
-    case 2:
-        evalBinContRes();
-        break;
-    case 3:
-        evalImgBasClass();
-        break;
-    case 4:
-        evalImgBasClassContRes();
-        break;
-    case 5:
-        std::cout << "exiting program" << std::endl;
-        exit(0);
-    default:
-        std::cout << "You have entered an incorrect option...\n";
-        break;
+        int userSelection = 0;
+
+        std::cout << "Please select one of the following options: " << std::endl;
+        std::cout << "\n(1) Evaluating a binary classifier"
+                  << "\n(2) Evaluating a binary classifier with a continuous response"
+                  << "\n(3) Evaluating image-based classifiers"
+                  << "\n(4) Evaluating image-based classifiers with continuous responses"
+                  << "\n(5) Exit Program"
+                  << std::endl;
+
+        std::cout << "Please enter a selection: ";
+        std::cin >> userSelection;
+
+        switch (userSelection)
+        {
+        case 1:
+            option_1();
+            continue;
+        case 2:
+            option_2();
+            continue;
+        case 3:
+            option_3();
+            continue;
+        case 4:
+            option_4();
+            continue;
+        case 5:
+            std::cout << "exiting program" << std::endl;
+            exit(0);
+        default:
+            std::cout << "You have entered an incorrect option...\n";
+            continue;
+        }
+
+        fflush(stdin);
     }
 
     return 0;
