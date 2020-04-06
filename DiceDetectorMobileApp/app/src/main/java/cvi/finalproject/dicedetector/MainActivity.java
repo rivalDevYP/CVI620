10e92f3f95c76952f762538289ee2b4db3a187a4
rivalDevYP <yathavanparamesh1998@hotmail.com>
2020-04-05 20:42:48 -0400

    added android build files

---
+++ DiceDetectorMobileApp/app/src/main/java/cvi/finalproject/dicedetector/MainActivity.java
@@ -0,0 +1,18 @@
+package cvi.finalproject.dicedetector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
