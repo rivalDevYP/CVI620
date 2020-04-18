b55fbbe3574ec08fe3ea9e1fc64885faa66a64a6
rivalDevYP <yathavanparamesh1998@hotmail.com>
2020-04-18 19:24:06 -0400

    added files

---
+++ Final_Assessment/braille.cpp
@@ -0,0 +1,17 @@
+/*****************************************/
+/* Task:        Project 2-1, Part 1      */
+/* Course:      CVI620                   */
+/* Student:     Yathavan Parameshwaran   */
+/* Date:        April 16, 2020           */
+/* Professor:   Vida Movahedi            */
+/*****************************************/
+
+#include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+
+int main ()
+{
+    
+    return 0;
+}
