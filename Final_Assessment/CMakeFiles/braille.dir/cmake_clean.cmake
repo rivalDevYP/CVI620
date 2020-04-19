29c4e893d958bf72a88e8dfcefac0661840d567f
rivalDevYP <yathavanparamesh1998@hotmail.com>
2020-04-19 13:40:59 -0400

    reached 37th hour of awakeness. cannot continue.

---
+++ Final_Assessment/CMakeFiles/braille.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/braille.dir/braille.cpp.o"
+  "braille"
+  "braille.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/braille.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Final_Assessment/braille.cpp
+++ Final_Assessment/braille.cpp
@@ -12,6 +12,40 @@
 
 int main ()
 {
+    cv::Mat sub1, sub2, sub1_grey, sub2_grey;
+
+    cv::namedWindow("sub1_window");
+    cv::namedWindow("sub2_window");
+
+    sub1 = cv::imread("project_files_dir/Project_2_1_files/SAM1_sub1.jpg", cv::IMREAD_ANYCOLOR);
+    sub2 = cv::imread("project_files_dir/Project_2_1_files/SAM1_sub1.jpg", cv::IMREAD_ANYCOLOR);
+
+    cv::cvtColor(sub1, sub1_grey, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(sub2, sub2_grey, cv::COLOR_BGR2GRAY);
+
+    cv::blur(sub1_grey, sub1_grey, cv::Size(3,3));
+    cv::threshold(sub1_grey, sub1_grey, 150, 255, cv::THRESH_BINARY);
+    cv::Canny(sub1_grey, sub1_grey, 120, 255);
+
+    cv::imshow("sub1_window", sub1_grey);
+    cv::imshow("sub2_window", sub2_grey);
+
+    while(1)
+    {
+        int keycode = cv::waitKey(0);
+        if (keycode == 120)
+        {
+            exit(0);
+        }
+        else
+        {
+            std::cout << "press 'x' to exit!" << std::endl;
+        }
+    }
     
+
+
+    
+
     return 0;
 }
